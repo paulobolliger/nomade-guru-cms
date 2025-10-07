10fb08499401d5ce87e8c018cf1f0c42de5bca14
paulobolliger <paulobolliger@gmail.com>
2025-10-07 10:10:50 -0300

    Versão inicial do Strapi CMS

---
+++ src/api/roteiro/controllers/roteiro.ts
@@ -0,0 +1,7 @@
+/**
+ * roteiro controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::roteiro.roteiro');
---
+++ src/api/roteiro/routes/roteiro.ts
@@ -0,0 +1,7 @@
+/**
+ * roteiro router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::roteiro.roteiro');
